c7026cff81d4d13539f4e76a06edc04720a37511
Ella199 <105546208+Ella199@users.noreply.github.com>
2022-07-15 15:12:54 +0200

    read dataset update

--- src/Scatterplot.elm
+++ src/Scatterplot.elm
@@ -71,11 +71,11 @@ getCsv x =
         |> List.map
             (\data ->
                     Http.get
-                        { url = "https://raw.githubusercontent.com/Ella199/Elm_Project-Student-Alcohol-Consumption/main/Data/CSV_Daten/.csv" ++ data
+                        { url = "https://raw.githubusercontent.com/Ella199/Elm_Project-Student-Alcohol-Consumption/main/Data/CSV_Daten/" ++ data
                         , expect = Http.expectString x
                         }
-                )
-            |> Cmd.batch
+            )
+        |> Cmd.batch
 
 list : List String 
 list = 
