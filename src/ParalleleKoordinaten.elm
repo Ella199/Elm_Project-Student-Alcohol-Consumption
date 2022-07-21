2d347ce209a894153dc72e929c89aed29d0c7371
Ella199 <105546208+Ella199@users.noreply.github.com>
2022-07-21 20:47:11 +0200

    Add svg

--- src/ParalleleKoordinaten.elm
+++ src/ParalleleKoordinaten.elm
@@ -114,7 +114,191 @@ decodingStudentAcoholConsumption =
                 |> Csv.Decode.andMap (Csv.Decode.field "walc"(String.toFloat >> Result.fromMaybe "error parsing string"))
             )
 
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        GotText result ->
+            case result of
+                Ok fullText ->
+                    ( Success <| { data = StudentAcoholConsumptionList [ fullText ], firstFUNCTION = .firstperiodGradeMath, secondFUNCTION = .secondperiodGradeMath, thirdFUNCTION = .thirdperiodGradeMath, fourthFUNCTION = firstperiodGradePort, firstNAME = "first period Grade mathematics", secondNAME = "second period Grade mathematics", thirdNAME = "third period Grade mathematics", fourthNAME = "third period Grade portuguese"}, Cmd.none )
 
+                Err  _ ->
+                    ( model, Cmd.none )
 
+        ChangeONE (x, a) ->
+            case model of 
+                ( Success <| { data = m.data, firstFUNCTION = x, secondFUNCTION = m.secondFUNCTION, thirdFUNCTION = m.thirdFUNCTION, fourthFUNCTION = m.fourthFUNCTION, firstNAME = x, secondNAME = m.secondNAME, thirdNAME = m.thirdNAME, fourthNAME = m.fourthNAME}, Cmd.none )
+                _ ->
+                    ( model, Cmd.none )
+        ChangeTWO (y, a) ->
+            case model of
+                Success m ->
+                    ( Success <| { data = m.data, firstFunction = m.firstFUNCTION, secondFUNCTION = y, thirdFUNCTION = m.thirdFUNCTION, fourthFUNCTION = m.fourthFUNCTION , firstNAME = m.firstNAME, secondNAME = a, thirdNAME = m.thirdNAME, fourthNAME = m.fourthNAME}, Cmd.none )
 
+                _ ->
+                    ( model, Cmd.none )
+        ChangeTHREE (z, a) ->
+            case model of
+                Success m ->
+                    ( Success <| { data = m.data, firstFUNCTION = m.firstFUNCTION, secondFUNCTION = m.secondFUNCTION, thirdFUNCTION = z, fourthFUNCTION = m.fourthFUNCTION, firstNAME = m.firstNAME, secondNAME = m.secondNAME, thirdNAME = a, fourthNAME = m.fourthNAME}, Cmd.none )
+
+                _ ->
+                    ( model, Cmd.none )
+        ChangeFOUR (c, a) ->
+            case model of
+                Success m ->
+                    ( Success <| { data = m.data, firstFUNCTION = m.firstFUNCTION, secondFUNCTION = m.secondFUNCTION, thirdFUNCTION = m.thirdFUNCTION, fourthFFUNCTION = c , firstNAME = m.firstNAME, secondNAME = m.secondNAME, thirdNAME = m.thirdNAME, fourthNAME = a}, Cmd.none )
+
+                _ ->
+                    ( model, Cmd.none )     
+
+ StudentAcoholConsumptionList :List String -> List StudentAcoholConsumption    
+ StudentAcoholConsumption List1 =
+    List.map(\t -> csvStringToData t) list1
+        |> List.concat            
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+padding : Float
+padding =
+    60
+
+radius : Float
+radius =
+    5.0
+
+tickCount : Int
+tickCount =
+    8
+
+defaultExtent : ( number, number1 )
+defaultExtent =
+    ( 0, 100 )
+
+wideExtent : List Float -> ( Float, Float )
+wideExtent values =
+    let
+        closeExtent =
+            Statistics.extent values
+                |> Maybe.withDefault defaultExtent
+
+        extent =
+            (Tuple.second closeExtent - Tuple.first closeExtent) / toFloat (2 * tickCount)
+    in
+    ( Tuple.first closeExtent - extent |> max 0
+    , Tuple.second closeExtent + extent
+    )
+
+parallelCoordinatesPlot : Float -> Float -> MultiDimData -> Svg msg
+parallelCoordinatesPlot w ar model =
+    let
+        h : Float
+        h =
+            w / ar
+
+        listTransformieren : List (List Float)
+        listTransformieren =
+            model.data
+                |> List.concat
+                |> List.map .value
+                |> List.Extra.transpose
+
+        listWideExtent : List ( Float, Float )
+        listWideExtent =
+            listTransformieren |> List.map wideExtent
+
+        listScale =
+            List.map (Scale.linear ( h, 0 )) listWideExtent
+
+        listAxis =
+            List.map (Axis.left [ Axis.tickCount tickCount ]) listScale
+
+        xScale =
+            Scale.linear ( 0, w ) ( 1, List.length model.dimDescription |> toFloat )
+    in
+    svg
+        [ viewBox 0 0 (w + 2 * padding) (h + 2 * padding)
+        , TypedSvg.Attributes.width <| TypedSvg.Types.Percent 90
+        , TypedSvg.Attributes.height <| TypedSvg.Types.Percent 90
+        ]
+    <|
+        [ TypedSvg.style []
+            [
+                TypedSvg.Core.text """
+                .parallelPoint { stroke: rgba(1, 0, 0,0.2);}
+                .parallelPoint:hover {stroke: rgb(60, 179, 113); stroke-width: 2;} 
+                .parallelPoint text { display: none; }
+                .parallelPoint:hover text { display: inline; stroke: rgb(0, 0, 0); stroke-width: 0.1; font-size: small; font-family: calibri}  
+                """
+            ]
+        , g [ TypedSvg.Attributes.class [ "parallelAxis" ] ]
+            [ g [ transform [ Translate (padding - 1) padding ] ] <|
+                List.indexedMap
+                    (\i axis ->
+                        g
+                            [ transform
+                                [ Translate (Scale.convert xScale (toFloat i + 1)) 0
+                                ]
+                            ]
+                            [ axis ]
+                    )
+                    listAxis
+            , g [ transform [ Translate (padding - 1) 0 ] ] <|
+                List.indexedMap
+                    (\i desc ->
+                        text_
+                            [ fontFamily [ "Times New Roman" ]
+                            , fontSize (Px 12)
+                            , x <| Scale.convert xScale (toFloat i + 1)
+                            , y <| padding * 7 / 8
+                            , textAnchor AnchorMiddle
+                            ]
+                            [ TypedSvg.Core.text desc ]
+                    )
+                    model.dimDescription
+            ]
+        ]
+            ++ (let
+                    drawPoint p name description =
+                        let
+                            linePath : Path.Path
+                            linePath =
+                                List.map3
+                                    (\desc s px ->
+                                        Just
+                                            ( Scale.convert xScale <| toFloat desc
+                                            , Scale.convert s px
+                                            )
+                                    )
+                                    (List.range 1 (List.length model.dimDescription))
+                                    listScale
+                                    p
+                                    |> Shape.line Shape.linearCurve
+                        in
+                        g [class ["parallelPoint"]][
+                            Path.element linePath
+                            [ stroke <| Paint <| Color.rgba 0 0 0 0.8
+                            , strokeWidth <| Px 0.5
+                            , fill PaintNone
+                            , class ["parallelPoint"]
+                            ]
+                            , text_
+                                [ x 300
+                                , y -20
+                                , TypedSvg.Attributes.textAnchor AnchorMiddle
+                                ]
+                                [ TypedSvg.Core.text (name++ (String.concat<|(List.map2(\a b-> ", " ++b++ ": "++ (String.fromFloat a))p description)))]
+                                
+                        ]
+                        
+                in
+                model.data
+                    |> List.map
+                        (\dataset ->
+                            g [ transform [ Translate (padding - 1) padding ] ]
+                                (List.map (\a -> drawPoint a.value a.pointName model.dimDescription) dataset)
+                        )
+               )
 
