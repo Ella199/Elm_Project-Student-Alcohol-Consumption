ce89f5a473fe66f13da4ef17ba00add41a839483
Ella199 <105546208+Ella199@users.noreply.github.com>
2022-08-11 21:45:37 +0200

    update VisualisationStickfigurePlot

--- src/VisualisationStickfigurePlot.elm
+++ src/VisualisationStickfigurePlot.elm
@@ -17,3 +17,22 @@ import Csv.Decode
 import Statistics
 import Html.Attributes as HA
 
+w : Float
+w =
+    900
+
+h : Float
+h =
+    450
+
+padding : Float
+padding =
+    60
+
+radius : Float
+radius =
+    5.0
+
+tickCount : Int
+tickCount =
+    5
