a251a4f29475af06c81b4e80cf59cd668a6fc535
Ella199 <105546208+Ella199@users.noreply.github.com>
2022-08-11 21:43:03 +0200

    Add VisualisationStickfigurePlot.elm

---
+++ src/VisualisationStickfigurePlot.elm
@@ -0,0 +1,19 @@
+module VisualisationStickfigurePlot exposing (..)
+
+import Axis
+import Html exposing (Html)
+import Scale exposing (ContinuousScale)
+import TypedSvg exposing (g, polyline, style, svg, text_)
+import TypedSvg.Attributes as TSA exposing (class, fontSize, textAnchor, transform, viewBox)
+import TypedSvg.Attributes.InPx exposing (x, y)
+import TypedSvg.Core exposing (Svg, text)
+import TypedSvg.Types as TST exposing (AnchorAlignment(..), FontWeight(..), Length(..), Transform(..), px) 
+import Browser
+import Html exposing (div, h1, p, button)
+import Html.Events exposing (onClick)
+import Http
+import Csv
+import Csv.Decode
+import Statistics
+import Html.Attributes as HA
+
